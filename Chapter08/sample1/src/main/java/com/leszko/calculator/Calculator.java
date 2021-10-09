5cd742a3f69bee08f2f0bdf7995f58751383fcad
alexr0g <91028238+alexr0g@users.noreply.github.com>
2021-10-09 18:49:46 -0400

    Added Javadoc comment

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -3,6 +3,11 @@ package com.leszko.calculator;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+/**
+@author Alex
+@version 1.1
+*/
+
 @Service
 public class Calculator {
         final static int umlNUMBER1 = 3;
